be5b0d21a2601374bd2a8e1852bf2b377bdfd339
thisconnect <p7tryk@gmail.com>
2020-10-09 13:02:02 +0200

    moja pierwsza aplikacja

---
+++ zajecia1/app/src/main/java/pl/pusb/kaniewski/myaplication/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.pusb.kaniewski.myaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
